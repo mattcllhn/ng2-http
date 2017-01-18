af5cbbd969e8524a6256c1f34747e24a4f171d63
Matt Callahan <mattcllhn@gmail.com>
2017-01-18 13:04:26 -0600

    http calling info to dom

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {Http} from '@angular/http';
+import {User} from './models/user';
 @Component({
   selector: 'my-app',
   styles: [`
-    
+
   `],
   template: `
     <div class="jumbotron text-center">
       <h1>The App Lives!</h1>
       <p>{{ message }}</p>
     </div>
+    <div *ngIf = "users">
+      <div *ngFor = "let user of users" >
+        <h2>{{user.first_name}} {{user.last_name}}</h2>
+        
+      </div>
+    </div>
   `
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  users: User[];
+
   message = 'This is the sample message.';
+  constructor(private http:Http){
+
+  }//constructor
+  ngOnInit(){
+    //get users
+    this.http.get('https://reqres.in/api/users?page=2')
+    .subscribe(data => {
+      console.log(data.json());
+      this.users = data.json().data;
+    })
+  }//ngoninit
 }
